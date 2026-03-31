da8ca315f5ca9a0836d1e7cc6d574d5e9289e508
hoshino <2493088050@qq.com>
2026-03-31 18:18:19 +0800

    sample

--- lora.py
+++ lora.py
@@ -1,38 +1,37 @@
 from ultralytics import RTDETR,YOLO
 from ultralytics.nn import AddModules
-
-
-from ultralytics import RTDETR,YOLO
-from ultralytics.nn import AddModules
-
-
 import warnings
+
 warnings.filterwarnings('ignore')
 # 实时保存日志，断网可训↓↓↓  
-# 终端运行命令：nohup python train.py > logs/DMFNet.txt 2>&1 & echo $! > logs/train.pid && tail -f logs/DMFNet.txt
+# 终端运行命令：nohup python train.py > logs/DMFNet_Finetune.txt 2>&1 & echo $! > logs/train_finetune.pid && tail -f logs/DMFNet_Finetune.txt
 
-model = YOLO("improve_multimodal/DMFNet_MDAFP3D_DySample.yaml")
+model = YOLO("results/DMFNet_MDAFP3D/weights/best.pt")
 
 model.train(
     task='detect',
     mode='train',
     data='data.yaml',
-    epochs=150,
+    
+    epochs=80,
     time=None,
     patience=30,
+    
     batch=24,
     imgsz=640,
+    
     save=True,
     save_period=-1,
     cache=False,
     device='0',
-    workers=8,
+    workers=4,
     optimizer='auto',
     verbose=True,
     seed=0,
     deterministic=True,
     single_cls=False,
     rect=False,
+    
     cos_lr=True,
     close_mosaic=10,
     resume=False,
@@ -81,15 +80,18 @@ model.train(
     opset=None,
     workspace=4,
     nms=False,
-    lr0=0.01,
+    
+    # 🔴 微调学习率要下降
+    lr0=0.005,         # 🌟 优化：从 0.01 降为 0.005
     lrf=0.01,
     momentum=0.937,
     weight_decay=0.0005,
     warmup_epochs=3.0,
     warmup_momentum=0.8,
     warmup_bias_lr=0.1,
+    
     box=7.5,
-    cls=0.5,
+    cls=1.5,
     dfl=1.5,
     pose=12.0,
     kobj=1.0,
@@ -98,12 +100,13 @@ model.train(
     hsv_h=0.015,
     hsv_s=0.7,
     hsv_v=0.4,
-    degrees=180.0,
+    
+    degrees=180.0,     # 🌟 优化：从 0.0 改为 180.0，适应航拍下的任意车辆朝向
     translate=0.1,
     scale=0.5,
     shear=0.0,
     perspective=0.0,
-    flipud=0.5,
+    flipud=0.5,        # 🌟 优化：增加上下翻转 (从 0.0 改为 0.5)
     fliplr=0.5,
     bgr=0.0,
     mosaic=0.5,
@@ -111,15 +114,14 @@ model.train(
     copy_paste=0.0,
     copy_paste_mode='flip',
     auto_augment='randaugment',
-    erasing=0.4,
+    erasing=0.0,
     crop_fraction=1.0,
     cfg=None,
     tracker='botsort.yaml',
+    
     project="results",
-    name="DMFNet_MDAFP3D",
+    name="DMFNet_MDAFP3D_Finetune_960", 
     exist_ok=True,
     pretrained=False,
-    save_dir='results/DMFNet_MDAFP3D',
 )
-print("✅  模型训练完成！")
-
+print("✅ 模型微调训练完成！")
